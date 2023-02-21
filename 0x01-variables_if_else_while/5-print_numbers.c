9aa300d4d06cf35095b1157d6373c7355e582d24
Imouran <cheikimouranouedraogo@example.com>
2023-02-20 16:29:43 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there*/
+/**
+ * main - is the main function
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
